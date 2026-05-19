114e2f1683bc63b51aa473c5035453afe7c08b53
Elijah Rosier <elijah@Elijahs-MacBook-Pro.local>
2026-05-19 12:50:09 +0100

    update css rendering and fieldData replacements

--- src/components/SEWidgetCustomEventList.tsx
+++ src/components/SEWidgetCustomEventList.tsx
@@ -3,20 +3,24 @@ import { useMemo, useRef, useEffect } from "preact/hooks";
 export const SEWidgetCustomEventList = (widget) => {
   const iframeRef = useRef<HTMLIFrameElement>(null);
 
-  const srcDoc = useMemo(
-    () => `<!DOCTYPE html>
+  const srcDoc = useMemo(() => {
+    let css = widget.variables.css || "";
+    console.log(Object.entries(widget.variables.fieldData));
+    Object.entries(widget.variables.fieldData).forEach(([key, value]) => {
+      css = css.replaceAll(`{{${key}}}`, value);
+    });
+    return `<!DOCTYPE html>
       <html>
         <head>
           <meta charset="UTF-8" />
-          <style>${widget.variables.css}</style>
+          <style>${css}</style>
         </head>
         <body style="margin:0;overflow:hidden;">
           ${widget.variables.html}
           <script>${widget.variables.js}<\/script>
         </body>
-      </html>`,
-    [widget.variables.html, widget.variables.css, widget.variables.js],
-  );
+      </html>`;
+  }, [widget.variables.html, widget.variables.css, widget.variables.js]);
 
   useEffect(() => {
     const iframe = iframeRef.current;
